a31af20053d70a8e97bfb13caec12a152fca32ea
olorun <olorun@ac3293f1-3019-6347-afe2-6dc4688bcd4e>
2012-11-26 23:18:11 +0000

    가까이 가고 떨어짐에 따라 인터렉션

---
+++ Assets/Script/Symbol/NoticePlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SymbolState))]
+public class NoticePlayer : MonoBehaviour
+{
+    public float distance = 2.0f;
+    private bool isPressReady;
+
+    private void Start()
+    {
+        isPressReady = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance))
+        {
+            //플레이어가 다가오고, 누름 준비상태가 아닐 때
+            //누름 준비상태로 만든다.
+            if (isColliderPlayer(hit) && !isPressReady)
+            {
+                isPressReady = true;
+                gameObject.SendMessage("PressReady");
+                //Player에게 Dispatch
+                //hit.collider.gameObject.SendMessage("PressReady");
+            }
+        }
+        //플레이어가 떨어져 있고, 누름 준비상태일 때
+        //누름 준비상태를 해제한다.
+        else if (isPressReady)
+        {
+            isPressReady = false;
+            gameObject.SendMessage("PressFailed");
+        }
+    }
+
+    private bool isColliderPlayer(RaycastHit hit)
+    {
+        Player player = hit.collider.gameObject.GetComponent<Player>();
+        return player != null;
+    }
+}
---
+++ Assets/Script/Symbol/SymbolState.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using UnityEngine;
+
+public enum eSymbolState
+{
+    DISABLE,
+    ACTIVE,
+    HOVER,
+    NORMAL,
+}
+
+public partial class SymbolState : MonoBehaviour
+{
+    public eSymbolState state = eSymbolState.NORMAL;
+    //state의 변화를 감지한다.
+    private eSymbolState prev_state;
+}
+
+public partial class SymbolState : MonoBehaviour
+{
+    private void _changeColor()
+    {
+        switch (state)
+        {
+            case eSymbolState.DISABLE:
+                GetComponent<tk2dSprite>().color = Color.gray;
+                break;
+            case eSymbolState.NORMAL:
+                GetComponent<tk2dSprite>().color = Color.white;
+                break;
+            case eSymbolState.HOVER:
+                GetComponent<tk2dSprite>().color = Color.yellow;
+                break;
+            case eSymbolState.ACTIVE:
+                GetComponent<tk2dSprite>().color = Color.green;
+                break;
+        }
+    }
+    //State Pattern을 적용해볼까
+    private void PressReady()
+    {
+        switch (state)
+        {
+            case eSymbolState.NORMAL:
+                state = eSymbolState.HOVER;
+                break;
+            case eSymbolState.HOVER:
+            case eSymbolState.DISABLE:
+            case eSymbolState.ACTIVE:
+                break;
+        }
+    }
+    private void PressFailed()
+    {
+        switch (state)
+        {
+            case eSymbolState.HOVER:
+                state = eSymbolState.NORMAL;
+                break;
+            case eSymbolState.NORMAL:
+            case eSymbolState.DISABLE:
+            case eSymbolState.ACTIVE:
+                break;
+        }
+    }
+    private void PressSuccess()
+    {
+        switch (state)
+        {
+            case eSymbolState.HOVER:
+                state = eSymbolState.ACTIVE;
+                break;
+            case eSymbolState.NORMAL:
+            case eSymbolState.DISABLE:
+            case eSymbolState.ACTIVE:
+                break;
+        }
+    }
+}
+public partial class SymbolState : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prev_state != state)
+        {
+            _changeColor();
+            prev_state = state;
+        }
+    }
+}
